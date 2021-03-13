1dbdb99551bc25a5737e35adb2bc2a2b09df9353
Kosh42 <kosh42efg@gmail.com>
2021-03-13 13:39:10 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+#Go to home
+cd ~
+#Clear old install
+rm -r -f FPP-JSON-Files/
+#Clone files
 git clone http://github.com/Kosh42/FPP-JSON-Files
-
+#Move 
 cd /opt/fpp/capes/bbb/strings
 
-sudo cp ~/FPP-JSON-Files/* ./
+sudo cp ~/FPP-JSON-Files/*.json /opt/fpp/capes/bbb/strings/
 
 echo "Done!"
